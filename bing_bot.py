39fb20e9ad281111df81ee177144676c102d21cb
connect-dots <35397054+connect-dots@users.noreply.github.com>
2018-09-07 12:28:17 +0100

    Update bing_bot.py

--- bing_bot.py
+++ bing_bot.py
@@ -100,7 +100,7 @@ class BingBot(object):
             rand_word = self.get_rand_search_term()
             self.driver.get("https://www.bing.com/search?q=" + rand_word)
             count += 1
-        sys.stdout.write('\n' "Completed" + str(count) + " out of " + str(self.num_searches_to_perform) + "Searches")
+        sys.stdout.write('\n' "Completed" + str(count) + " Searches")
         sys.stdout.flush()
 
     def quit(self):
