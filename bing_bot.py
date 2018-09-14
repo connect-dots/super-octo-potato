9990b1330b4d3cb4be46f845d1f3aeceadd60e23
connect-dots <35397054+connect-dots@users.noreply.github.com>
2018-09-14 17:03:22 +0100

    Update bing_bot.py

--- bing_bot.py
+++ bing_bot.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 
 import csv
 import os
-import random
-import time
 import sys
 
 working = os.path.dirname(os.path.abspath(__file__))
@@ -17,18 +13,16 @@ class BingBot(object):
     """ Automatically login to Bing and search random words
     to accumulate rewards """
 
-    def __init__(self, email, password, is_mobile=False):
+    def __init__(self, email, password):
 
         self.login_url = "https://login.live.com/"
         self.bing_url = "https://www.bing.com/"
-        self.dash_url = "https://account.microsoft.com/rewards/"
+        self.dash_url = "https://account.microsoft.com/"
 
         self.word_list = os.path.join(working, "word_list.txt")
 
         self.email = email
         self.password = password
-
-        self.is_mobile = is_mobile
 
         self.explicit_wait = 10
         self.min_search_wait = 5
@@ -36,18 +30,7 @@ class BingBot(object):
 
         self.num_searches_to_perform = 32
 
-        self.profile = self.firefox_profile()
-        self.options = Options()
-        self.options.add_argument("--headless")
-        self.driver = webdriver.Firefox(firefox_profile=self.profile, firefox_options=self.options)
-
-    def firefox_profile(self):
-        """Searches on mobile as well as desktop"""
-        profile = webdriver.FirefoxProfile()
-        if self.is_mobile:
-            profile.set_preference("general.useragent.override",
-                                   "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0")
-        return profile
+        self.driver = webdriver.Firefox()
 
     def login(self):
         """Automatically log into Bing Account."""
@@ -75,47 +58,13 @@ class BingBot(object):
     def get_points_start(self):
         """Get current user points"""
         self.driver.get(self.dash_url)
+        self.driver.implicitly_wait(5)
+        self.driver.get(self.dash_url)
+        self.driver.implicitly_wait(5)
 
-        wait = WebDriverWait(self.driver, self.explicit_wait)
-        wait.until(EC.presence_of_all_elements_located((By.ID, "msame_si3")))
+        points = self.driver.find_element_by_xpath("/html[@class='ltr home-index home js picture eventlistener']/body/div[@id='page-wrapper']/div[@id='main-content-landing']/main[@id='home-index']/div[@id='home-app-host']/div[@class='ng-scope']/home-page[@class='ng-scope ng-isolate-scope']/div[1]/home-banner[@class='ng-isolate-scope']/mee-banner[@class='home-banner ng-scope ng-isolate-scope']/div[@class='amc-banner theme-dark']/div/div[@class='space-3-col ng-scope']/div[@class='info-columns']/div[@class='info-column ng-scope'][2]/mee-banner-slot-3[@class='ng-scope']/banner-rewards[@class='ng-isolate-scope']/mee-progress-view[@class='ng-isolate-scope']/div[4]/finished-view[@class='ng-scope']/mee-banner-media[@class='ng-scope']/media-body/p[@class='ng-binding'][1]")
 
-        points = self.driver.find_element_by_xpath("/html[@class='ltr rewards-oneuidashboard rewards js picture eventlistener']/body/div[@id='meControlDropdown']/div[@class='msame_Drop_content']/div[@class='msame_Drop_rewards']/div[@id='msame_si3']/a[@class='msame_TxtTrunc']")
-
-        sys.stdout.write(str(points))
-
-    def get_rand_search_time(self):
-        """Randomize time for searches"""
-        return random.uniform(self.min_search_wait, self.max_search_wait)
-
-    def get_rand_search_term(self):
-        """Randomly select search term for Bing from word list"""
-        total_bytes = os.stat(self.word_list).st_size
-        random_point = random.randint(0, total_bytes)
-        file = open(self.word_list)
-        file.seek(random_point)
-        file.readline()
-        return file.readline()
-
-    def bing_search(self):
-        """Navigate to Bing and perform random searches."""
-        self.driver.get(self.bing_url)
-
-        wait = WebDriverWait(self.driver, self.explicit_wait)
-        wait.until(EC.presence_of_all_elements_located((By.ID, "sb_form_q")))
-
-        count = 0
-        for search in range(self.num_searches_to_perform):
-            sys.stdout.write('\r')
-            sys.stdout.write("Searching: " + str(count) + " out of " + str(self.num_searches_to_perform))
-            sys.stdout.flush()
-            rand_time = self.get_rand_search_time()
-            time.sleep(rand_time)
-
-            rand_word = self.get_rand_search_term()
-            self.driver.get("https://www.bing.com/search?q=" + rand_word)
-            count += 1
-        sys.stdout.write('\n' "Completed" + str(count) + " Searches")
-        sys.stdout.flush()
+        print(points)
 
     def quit(self):
         """Close the browser."""
@@ -124,7 +73,7 @@ class BingBot(object):
     def run(self):
         """Run the primary login, search, quit protocol."""
         self.login()
-        self.bing_search()
+        self.get_points_start()
         self.quit()
 
 
@@ -136,14 +85,5 @@ with open(credentials, 'r') as f:
         user_id = row[0]
         password = row[1]
 
-        sys.stdout.write("Processing User via Desktop" '\n')
-        sys.stdout.write("---------------------------" '\n')
-        bing_bot = BingBot(user_id, password, is_mobile=True)
-        bing_bot.run()
-        sys.stdout.write('\n' '\n' "Desktop Searches Complete" '\n' '\n')
-
-        sys.stdout.write("Processing user via Mobile" '\n')
-        sys.stdout.write("--------------------------" '\n')
         bing_bot = BingBot(user_id, password)
         bing_bot.run()
-        sys.stdout.write('\n' '\n' "Mobile Searches Complete" '\n' '\n')
