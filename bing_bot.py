f9e13720a9f74e0fe878798273b8395d8bbc5815
connect-dots <35397054+connect-dots@users.noreply.github.com>
2018-10-11 10:09:12 +0100

    Update bing_bot.py

--- bing_bot.py
+++ bing_bot.py
@@ -46,7 +46,7 @@ class BingBot(object):
         profile = webdriver.FirefoxProfile()
         if self.is_mobile:
             profile.set_preference("general.useragent.override",
-                                   "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0")
+                                   "Mozilla/5.0 (Mobile; LYF/F90M/LYF-F90M-000-02-28-130318; Android; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.0")
         return profile
 
     def login(self):
